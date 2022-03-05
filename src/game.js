173860370f6e3d9609d005fafac3b29e0397a7e9
Anar Seferov <1anarseferov@gmail.com>
2022-03-05 14:06:34 +0400

    added movement for forg

--- src/game.js
+++ src/game.js
@@ -1,1 +1,28 @@
 import './main.css';
+
+// const displayTimeLeft = document.getElementById('time-left');
+// const displayGameResult = document.getElementById('result');
+// const startPauseBtn = document.getElementById('start-pause-btn');
+const squares = document.querySelectorAll('.game-container div');
+
+let currentIndex = 76;
+const width = 9;
+
+const frogMove = (e) => {
+    squares[currentIndex].classList.remove('frog');
+
+    if (e.key === 'ArrowUp' && currentIndex - width >= 0) {
+        currentIndex -= width;
+    } else if (e.key === 'ArrowDown' && currentIndex + width <= width * width) {
+        currentIndex += width;
+    } else if (e.key === 'ArrowRight' && currentIndex % width !== width - 1) {
+        currentIndex += 1;
+    } else if (e.key === 'ArrowLeft' && currentIndex % width !== 0) {
+        currentIndex -= 1;
+    }
+
+    squares[currentIndex].classList.add('frog');
+
+}
+
+document.addEventListener('keyup', frogMove);
