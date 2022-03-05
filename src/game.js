827b008c00c9f8959d15a3a2c3bf04723143bcbe
Anar Seferov <1anarseferov@gmail.com>
2022-03-05 16:12:03 +0400

    log movement added

--- src/game.js
+++ src/game.js
@@ -6,6 +6,8 @@ import './main.css';
 const squares = document.querySelectorAll('.game-container div');
 const carsLeft = document.querySelectorAll('.car-left');
 const carsRight = document.querySelectorAll('.car-right');
+const logsLeft = document.querySelectorAll('.log-left');
+const logsRight = document.querySelectorAll('.log-right');
 
 let currentIndex = 76;
 const width = 9;
@@ -29,23 +31,63 @@ const frogMove = (e) => {
 
 document.addEventListener('keyup', frogMove);
 
-const moveCar = (carLeft) => {
+const moveCar = (car) => {
 
-    if (carLeft.classList.contains('c1')) {
-        carLeft.classList.remove('c1');
-        carLeft.classList.add('c3');
-    } else if (carLeft.classList.contains('c2')) {
-        carLeft.classList.remove('c2');
-        carLeft.classList.add('c1');
-    } else if (carLeft.classList.contains('c3')) {
-        carLeft.classList.remove('c3');
-        carLeft.classList.add('c2');
+    if (car.classList.contains('c1')) {
+        car.classList.remove('c1');
+        car.classList.add('c3');
+    } else if (car.classList.contains('c2')) {
+        car.classList.remove('c2');
+        car.classList.add('c1');
+    } else if (car.classList.contains('c3')) {
+        car.classList.remove('c3');
+        car.classList.add('c2');
+    }
+}
+
+const moveLogsLeft = (log) => {
+    if (log.classList.contains('l1')) {
+        log.classList.remove('l1');
+        log.classList.add('l2');
+    } else if (log.classList.contains('l2')) {
+        log.classList.remove('l2');
+        log.classList.add('l3');
+    } else if (log.classList.contains('l3')) {
+        log.classList.remove('l3');
+        log.classList.add('l4');
+    } else if (log.classList.contains('l4')) {
+        log.classList.remove('l4');
+        log.classList.add('l5');
+    } else if (log.classList.contains('l5')) {
+        log.classList.remove('l5');
+        log.classList.add('l1');
+    }
+}
+
+const moveLogsRight = (log) => {
+    if (log.classList.contains('l1')) {
+        log.classList.remove('l1');
+        log.classList.add('l5');
+    } else if (log.classList.contains('l2')) {
+        log.classList.remove('l2');
+        log.classList.add('l1');
+    } else if (log.classList.contains('l3')) {
+        log.classList.remove('l3');
+        log.classList.add('l2');
+    } else if (log.classList.contains('l4')) {
+        log.classList.remove('l4');
+        log.classList.add('l3');
+    } else if (log.classList.contains('l5')) {
+        log.classList.remove('l5');
+        log.classList.add('l4');
     }
 }
 
 const autoMove = () => {
     carsLeft.forEach(carLeft => moveCar(carLeft));
     carsRight.forEach(carRight => moveCar(carRight));
+    logsLeft.forEach(logLeft => moveLogsLeft(logLeft));
+    logsRight.forEach(logRight => moveLogsRight(logRight));
 };
 
 setInterval(autoMove, 1000);
